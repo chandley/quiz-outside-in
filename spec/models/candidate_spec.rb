90a891eec4411cfb4b321cdcec46b9ee720a6e54
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-30 14:44:53 +0000

    failing test candidate starts off not having answered question

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Candidate, :type => :model do
   
+  it 'starts off not having answered a question' do
+    question =  FactoryGirl.create :question
+    candidate = FactoryGirl.create :candidate
+    expect(candidate.has_answered?(question)).to be(false)
+  end
+
+  it 'can answer a question' do
+
+  end
+
 end
